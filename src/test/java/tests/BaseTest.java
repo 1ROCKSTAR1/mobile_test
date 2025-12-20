5a2946302d6a40803ba8bca7791fe77844727010
Alex_299 <elrockstar25@gmail.com>
2025-12-20 15:55:42 +0300

    improved mobile tests were fixed (basetest)

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -9,17 +9,21 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 
 import static com.codeborne.selenide.Selenide.closeWebDriver;
+import static com.codeborne.selenide.Selenide.open;
 
 public class BaseTest {
 
     @BeforeAll
     static void beforeAll() {
         Configuration.browser = BrowserStackDriver.class.getName();
+        Configuration.browserSize = null;
+        Configuration.timeout = 30000;
     }
 
     @BeforeEach
     void beforeEach() {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+        open();
     }
     @AfterEach
     void afterEach() {
