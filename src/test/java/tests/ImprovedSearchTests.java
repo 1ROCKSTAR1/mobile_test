d4b703b3280f07273f386a5f547fa44fa0a65684
alexaastri <AVoronin@lanit.ru>
2025-12-22 14:04:55 +0300

    one more test was added

--- src/test/java/tests/ImprovedSearchTests.java
+++ src/test/java/tests/ImprovedSearchTests.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
 import static io.appium.java_client.AppiumBy.*;
@@ -29,5 +30,19 @@ public class ImprovedSearchTests extends BaseTest {
         $(id("org.wikipedia.alpha:id/search_src_text")).sendKeys(";lkj;lkhjljkgyy");
 
         $$(id("org.wikipedia.alpha:id/page_list_item_title")).shouldHave(sizeGreaterThan(0));
+    }
+
+    @Test
+    @DisplayName("Первый мобильный тест на мобилку с appium+selenide (поиск статьи)")
+    void successfulSearchTestArticle() {
+
+        $(accessibilityId("Search Wikipedia")).click();
+        $(id("org.wikipedia.alpha:id/search_src_text")).sendKeys("Formula-1");
+
+        $$(id("org.wikipedia.alpha:id/page_list_item_title"))
+                .get(0)
+                .click();
+
+        $(id("pcs-edit-section-title-description")).shouldHave(text("Competition"));
     }
 }
