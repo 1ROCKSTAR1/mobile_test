7db9cadafa383197844d87bb9f20e4cc487573b4
alexaastri <AVoronin@lanit.ru>
2025-12-24 11:35:25 +0300

    action skip was added

--- src/test/java/android/BaseAndroidTest.java
+++ src/test/java/android/BaseAndroidTest.java
@@ -4,13 +4,16 @@ import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import drivers.BrowserStackAndroidDriver;
 import helpers.Attach;
+import io.appium.java_client.AppiumBy;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 
-import static com.codeborne.selenide.Selenide.closeWebDriver;
-import static com.codeborne.selenide.Selenide.open;
+import java.time.Duration;
+
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.*;
 
 public class BaseAndroidTest {
 
@@ -25,6 +28,9 @@ public class BaseAndroidTest {
     void beforeEach() {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
         open(); // обязательный костыль №2
+        $(AppiumBy.id("org.wikipedia.alpha:id/fragment_onboarding_skip_button"))
+                .shouldBe(visible, Duration.ofSeconds(10))
+                .click();
     }
 
     @AfterEach
